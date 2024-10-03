d78e5605f0fa367fef6b55ef138178873ceb7c2b
saiboks <botssai69@gmail.com>
2024-10-04 02:11:21 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from AnonXMu.sic.utils.formatters import time_to_seconds
 
 
 
