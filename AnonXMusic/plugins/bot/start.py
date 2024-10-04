2ec4d95919cd3490163a00ca20a4e756180e0d79
saiboks <botssai69@gmail.com>
2024-10-04 10:41:36 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -82,7 +82,7 @@ async def start_pm(client, message: Message, _):
                     chat_id=config.LOGGER_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-        else:
+    else:
         out = private_panel(_)
         await message.reply_photo(
             photo=config.START_IMG_URL,
@@ -98,6 +98,7 @@ async def start_pm(client, message: Message, _):
 
 
         )  
+
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
