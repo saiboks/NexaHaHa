ee6147e20708f5887c26c8cbcccafca91621e78b
saiboks <botssai69@gmail.com>
2024-10-04 20:05:06 +0530

    Update cosplay.py

--- AnonXMusic/plugins/tools/cosplay.py
+++ AnonXMusic/plugins/tools/cosplay.py
@@ -1,13 +1,12 @@
 import requests
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, ChatType
 from AnonXMusic import app
 
 @app.on_message(filters.command("cosplay"))
 async def cosplay(_, msg):
     img = requests.get("https://waifu-api.vercel.app").json()
-    await msg.reply_photo(img, caption=f")
-
+    await msg.reply_photo(img, caption="Here is your cosplay image!")
 
 @app.on_message(filters.command("ncosplay"))
 async def ncosplay(_, msg):
@@ -15,4 +14,4 @@ async def ncosplay(_, msg):
         await msg.reply_text("❍ sᴏʀʀʏ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ʙᴏᴛ")
     else:
         ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
-        await msg.reply_photo(ncosplay, caption=f")
+        await msg.reply_photo(ncosplay, caption="Here is your ncosplay image!")
