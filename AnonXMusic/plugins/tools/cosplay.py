a9c4febb834c273c861bf0fabef2d7587ed69af2
saiboks <botssai69@gmail.com>
2024-10-04 20:27:05 +0530

    Update cosplay.py

--- AnonXMusic/plugins/tools/cosplay.py
+++ AnonXMusic/plugins/tools/cosplay.py
@@ -1,13 +1,12 @@
 import requests
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, ChatType
 from AnonXMusic import app
 
 @app.on_message(filters.command("cosplay"))
 async def cosplay(_, msg):
     img = requests.get("https://waifu-api.vercel.app").json()
-    await msg.reply_photo(img, caption=f"❖ ᴄᴏsᴘʟᴀʏ ʙʏ ~ 𝐀𝐫𝐭𝐡𝐞𝐲 ࿐")
-
+    await msg.reply_photo(img['url'], caption=f"❖ ᴄᴏsᴘʟᴀʏ ʙʏ ~ 𝐀𝐫𝐭𝐡𝐞𝐲 ࿐")
 
 @app.on_message(filters.command("ncosplay"))
 async def ncosplay(_, msg):
@@ -15,4 +14,7 @@ async def ncosplay(_, msg):
         await msg.reply_text("❍ sᴏʀʀʏ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ʙᴏᴛ")
     else:
         ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
-        await msg.reply_photo(ncosplay, caption=f"❖ ᴄᴏsᴘʟᴀʏ ʙʏ ~ 𝐀𝐫𝐭𝐡𝐞𝐲 ࿐")
+        if 'url' in ncosplay:
+            await msg.reply_photo(ncosplay['url'], caption=f"❖ ᴄᴏsᴘʟᴀʏ ʙʏ ~ 𝐀𝐫𝐭𝐡𝐞𝐲 ࿐")
+        else:
+            await msg.reply_text("❍ Failed to fetch cosplay image.")
