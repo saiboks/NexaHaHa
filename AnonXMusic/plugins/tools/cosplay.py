6b7fd33a1b70148785e6251c39cda41d69edbd27
saiboks <botssai69@gmail.com>
2024-10-04 19:49:09 +0530

    Update tools

---
+++ AnonXMusic/plugins/tools/cosplay.py
@@ -0,0 +1,18 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import Message
+from AnonXMusic import app
+
+@app.on_message(filters.command("cosplay"))
+async def cosplay(_, msg):
+    img = requests.get("https://waifu-api.vercel.app").json()
+    await msg.reply_photo(img, caption=f")
+
+
+@app.on_message(filters.command("ncosplay"))
+async def ncosplay(_, msg):
+    if msg.chat.type != ChatType.PRIVATE:
+        await msg.reply_text("❍ sᴏʀʀʏ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ʙᴏᴛ")
+    else:
+        ncosplay = requests.get("https://waifu-api.vercel.app/items/1").json()
+        await msg.reply_photo(ncosplay, caption=f")
