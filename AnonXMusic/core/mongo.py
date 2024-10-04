c1291dd23758a1240ba67675aba3d88bf6bf007a
saiboks <botssai69@gmail.com>
2024-10-04 13:25:24 +0530

    Update mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -2,8 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
-db = mongo.AnonXMusic
-
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
