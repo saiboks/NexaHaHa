c24578df0c902e1320b3c9d07a20a8e4cc780606
saiboks <botssai69@gmail.com>
2024-10-08 12:06:35 +0530

    Update restrict.py

--- AnonXMusic/plugins/tools/restrict.py
+++ AnonXMusic/plugins/tools/restrict.py
@@ -128,12 +128,8 @@ async def unmute_user(client, message):
                 permissions=ChatPermissions(
                     can_send_messages=True,
                     can_send_media_messages=True,
-                    can_send_stickers=True,
-                    can_send_animations=True,
-                    can_send_games=True,
-                    can_use_inline_bots=True,
-                    can_add_web_page_previews=True,
                     can_send_polls=True,
+                    can_add_web_page_previews=True,
                     can_change_info=True,
                     can_invite_users=True,
                     can_pin_messages=True
