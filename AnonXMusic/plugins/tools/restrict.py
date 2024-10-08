fd6326242fedbf9fc7e146c57277aa20a5fa2f5d
saiboks <botssai69@gmail.com>
2024-10-08 16:02:36 +0530

    Update restrict.py

--- AnonXMusic/plugins/tools/restrict.py
+++ AnonXMusic/plugins/tools/restrict.py
@@ -1,3 +1,4 @@
+import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatPermissions, CallbackQuery
 from pyrogram.enums import ChatMemberStatus
