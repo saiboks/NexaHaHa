5e7c5085b2409a16535060af4dd16e26b4318c66
saiboks <botssai69@gmail.com>
2024-10-11 08:58:11 +0530

    Update info.py

--- AnonXMusic/plugins/tools/info.py
+++ AnonXMusic/plugins/tools/info.py
@@ -1,6 +1,4 @@
 import asyncio, os, time, aiohttp
-from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
 from AnonXMusic import app
 from pyrogram import filters, Client, enums
@@ -15,52 +13,7 @@ EVAA = [
     ],
 ]
 
-get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
-resize_text = (
-    lambda text_size, text: (text[:text_size] + "...").upper()
-    if len(text) > text_size
-    else text.upper()
-)
-
 # --------------------------------------------------------------------------------- #
-
-
-async def get_userinfo_img(
-    bg_path: str,
-    font_path: str,
-    user_id: Union[int, str],    
-    profile_path: Optional[str] = None
-):
-    bg = Image.open(bg_path)
-
-    if profile_path:
-        img = Image.open(profile_path)
-        mask = Image.new("L", img.size, 0)
-        draw = ImageDraw.Draw(mask)
-        draw.pieslice([(0, 0), img.size], 0, 360, fill=255)
-
-        circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
-        circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((363, 363))
-        bg.paste(resized, (525, 52), resized)
-
-    img_draw = ImageDraw.Draw(bg)
-
-
-
-    path = f"./userinfo_img_{user_id}.png"
-    bg.save(path)
-    return path
-
-
-# --------------------------------------------------------------------------------- #
-
-bg_path = "AnonXMusic/assets/IN.PNG"
-font_path = "AnonXMusic/assets/hiroko.ttf"
-
-#
-# --------------------------------------------------------------------------------- #
-
 
 INFO_TEXT = """
 <u><b>ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b></u>
@@ -90,7 +43,6 @@ async def userstatus(user_id):
    except:
         return "**✦ sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ !**"
 
-
 # --------------------------------------------------------------------------------- #
 
 @app.on_message(filters.command(["info", "information", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
@@ -110,14 +62,7 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await app.download_media(user.photo.big_file_id)
-            welcome_photo = await get_userinfo_img(
-                bg_path=bg_path,
-                font_path=font_path,
-                user_id=user_id,
-                profile_path=photo,
-            )
-            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+            await app.send_message(chat_id, text=INFO_TEXT.format(
                 id, username, mention, status, dc_id), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))        
@@ -133,14 +78,7 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await app.download_media(user.photo.big_file_id)
-            welcome_photo = await get_userinfo_img(
-                bg_path=bg_path,
-                font_path=font_path,
-                user_id=user_id,
-                profile_path=photo,
-            )
-            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+            await app.send_message(chat_id, text=INFO_TEXT.format(
                 id, username, mention, status, dc_id), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))
@@ -158,17 +96,7 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await app.download_media(message.reply_to_message.from_user.photo.big_file_id)
-            welcome_photo = await get_userinfo_img(
-                bg_path=bg_path,
-                font_path=font_path,
-                user_id=user_id,
-                profile_path=photo,
-            )
-            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+            await app.send_message(chat_id, text=INFO_TEXT.format(
                 id, username, mention, status, dc_id), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))
-
-####
-
