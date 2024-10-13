332233208b762508238f900fd3c277860475600e
saiboks <botssai69@gmail.com>
2024-10-13 06:12:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+    "GIT_TOKEN", "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")  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/novauibot")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/novauibot")
