6a1efadb87598f7cfc69ba1e12f49cda689f6011
saiboks <botssai69@gmail.com>
2024-10-14 13:00:58 +0530

    Update ban.py

---
+++ AnonXMusic/plugins/tools/ban.py
@@ -0,0 +1,68 @@
+import asyncio
+from contextlib import suppress
+
+from pyrogram import filters
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
+from pyrogram.types import (
+    CallbackQuery,
+    ChatPermissions,
+    ChatPrivileges,
+    Message,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from string import ascii_lowercase
+from typing import Dict, Union
+
+from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.core.mongo import mongodb
+from utils.error import capture_err
+from AnonXMusic.utils.keyboard import ikb
+from AnonXMusic.utils.database import save_filter
+from AnonXMusic.utils.functions import (
+    extract_user,
+    extract_user_and_reason,
+    time_converter,
+)
+from utils.permissions import adminsOnly, member_permissions
+from config import BANNED_USERS
+
+warnsdb = mongodb.warns
+
+
+# kick
+@app.on_message(filters.command(["kick", "skick"]) & ~filters.private & ~BANNED_USERS)
+@adminsOnly("can_restrict_members")
+async def kickFunc(_, message: Message):
+    user_id, reason = await extract_user_and_reason(message)
+    if not user_id:
+        return await message.reply_text("ɪ ᴄᴀɴ'ᴛ ғɪɴᴅ ᴛʜᴀᴛ ᴜsᴇʀ")
+    if user_id == app.id:
+        return await message.reply_text("ɪ ᴄᴀɴ'ᴛ ᴋɪᴄᴋ ᴍʏsᴇʟғ, ɪ ᴄᴀɴ ʟᴇᴀᴠᴇ ɪғ ʏᴏᴜ ᴡᴀɴᴛ.")
+    if user_id in SUDOERS:
+        return await message.reply_text("ʏᴏᴜ ᴡᴀɴɴᴀ ᴋɪᴄᴋ ᴛʜᴇ ᴇʟᴇᴠᴀᴛᴇᴅ ᴏɴᴇ ?")
+    if user_id in [
+        member.user.id
+        async for member in app.get_chat_members(
+            chat_id=message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
+        )
+    ]:
+        return await message.reply_text(
+            "ɪ ᴄᴀɴ'ᴛ ᴋɪᴄᴋ ᴀɴ ᴀᴅᴍɪɴ, ʏᴏᴜ ᴋɴᴏᴡ ᴛʜᴇ ʀᴜʟᴇs, ʏᴏᴜ ᴋɴᴏᴡ ᴛʜᴇ ʀᴜʟᴇs, sᴏ ᴅᴏ ɪ "
+        )
+    mention = (await app.get_users(user_id)).mention
+    msg = f"""
+**ᴋɪᴄᴋᴇᴅ ᴜsᴇʀ:** {mention}
+**ᴋɪᴄᴋᴇᴅ ʙʏ:** {message.from_user.mention if message.from_user else 'ᴀɴᴏɴᴍᴏᴜs'}
+**ʀᴇᴀsᴏɴ:** {reason or 'ɴᴏ ʀᴇᴀsᴏɴ ᴘʀᴏᴠɪᴅᴇᴅ'}"""
+    await message.chat.ban_member(user_id)
+    replied_message = message.reply_to_message
+    if replied_message:
+        message = replied_message
+    await message.reply_text(msg)
+    await asyncio.sleep(1)
+    await message.chat.unban_member(user_id)
+    if message.command[0][0] == "s":
+        await message.reply_to_message.delete()
+        await app.delete_user_history(message.chat.id, user_id)
