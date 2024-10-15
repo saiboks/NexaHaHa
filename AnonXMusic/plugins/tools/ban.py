b4abac0792aa241b375b88efe5524ddd02029a18
saiboks <botssai69@gmail.com>
2024-10-16 04:51:53 +0530

    Update ban.py

--- AnonXMusic/plugins/tools/ban.py
+++ AnonXMusic/plugins/tools/ban.py
@@ -41,8 +41,8 @@ async def banFunc(_, message: Message):
 
     if not user_id:
         return await message.reply_text(
-            "ᴜsᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ.\n"
-            "ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ /ban ᴍᴜsᴛ ʙᴇ ᴜsᴇᴅ sᴘᴇᴄɪғʏɪɴɢ ᴜsᴇʀ ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ/ᴍᴇɴᴛɪᴏɴ ᴏʀ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴏɴᴇ ᴏғ ᴛʜᴇɪʀ ᴍᴇssᴀɢᴇs."
+            "<u><b>ᴜsᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ.</b></u>\n"
+            "ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ <b>/ban</b> ᴍᴜsᴛ ʙᴇ ᴜsᴇᴅ sᴘᴇᴄɪғʏɪɴɢ ᴜsᴇʀ <b>ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ/ᴍᴇɴᴛɪᴏɴ ᴏʀ ʀᴇᴘʟʏɪɴɢ</b> ᴛᴏ ᴏɴᴇ ᴏғ ᴛʜᴇɪʀ ᴍᴇssᴀɢᴇs."
         )
     if user_id == app.id:
         return await message.reply_text("I can't ban myself, i can leave if you want.")
@@ -78,3 +78,55 @@ async def banFunc(_, message: Message):
     if replied_message:
         message = replied_message
     await message.reply_text(msg)
+
+
+#sban
+@app.on_message(
+    filters.command(["sban"]) & ~filters.private & ~BANNED_USERS
+)
+@adminsOnly("can_restrict_members")
+async def banFunc(_, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+
+    if not user_id:
+        return await message.reply_text(
+            "<b><u>ᴜsᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ.</u></b>\n"
+            "ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ <b>/ban</b> ᴍᴜsᴛ ʙᴇ ᴜsᴇᴅ sᴘᴇᴄɪғʏɪɴɢ ᴜsᴇʀ <b>ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ/ᴍᴇɴᴛɪᴏɴ ᴏʀ ʀᴇᴘʟʏɪɴɢ</b> ᴛᴏ ᴏɴᴇ ᴏғ ᴛʜᴇɪʀ ᴍᴇssᴀɢᴇs."
+        )
+    if user_id == app.id:
+        return await message.reply_text("I can't ban myself, i can leave if you want.")
+    if user_id in SUDOERS:
+        return await message.reply_text("You Wanna Ban The Elevated One?, RECONSIDER!")
+    if user_id in [
+        member.user.id
+        async for member in app.get_chat_members(
+            chat_id=message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
+        )
+    ]:
+        return await message.reply_text(
+            "I can't ban an admin, You know the rules, so do i."
+        )
+
+    try:
+        mention = (await app.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            message.reply_to_message.sender_chat.title
+            if message.reply_to_message
+            else "Anon"
+        )
+
+    msg = (
+        f"<b>● ʙᴀɴɴᴇᴅ ᴜsᴇʀ ➠</b> {mention}\n"
+    )
+    if reason:
+        msg += f"<b>● ʀᴇᴀsᴏɴ ➠</b> {reason}"
+    
+    await message.chat.ban_member(user_id)
+    replied_message = message.reply_to_message
+    if replied_message:
+        message = replied_message
+    await message.reply_text(msg)
+
+    # Delete the command message after processing
+    await message.delete()
