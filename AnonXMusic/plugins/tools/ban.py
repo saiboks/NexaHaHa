46dcdd84fd52115a0193806108a9c8c047cf5c75
saiboks <botssai69@gmail.com>
2024-10-16 08:56:28 +0530

    Update ban.py

--- AnonXMusic/plugins/tools/ban.py
+++ AnonXMusic/plugins/tools/ban.py
@@ -31,6 +31,64 @@ from config import BANNED_USERS
 warnsdb = mongodb.warns
 
 
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = list(ascii_lowercase)[:10]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def get_warns_count() -> dict:
+    chats_count = 0
+    warns_count = 0
+    async for chat in warnsdb.find({"chat_id": {"$lt": 0}}):
+        for user in chat["warns"]:
+            warns_count += chat["warns"][user]["warns"]
+        chats_count += 1
+    return {"chats_count": chats_count, "warns_count": warns_count}
+
+
+async def get_warns(chat_id: int) -> Dict[str, int]:
+    warns = await warnsdb.find_one({"chat_id": chat_id})
+    if not warns:
+        return {}
+    return warns["warns"]
+
+
+async def get_warn(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    warns = await get_warns(chat_id)
+    if name in warns:
+        return warns[name]
+
+
+async def add_warn(chat_id: int, name: str, warn: dict):
+    name = name.lower().strip()
+    warns = await get_warns(chat_id)
+    warns[name] = warn
+
+    await warnsdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"warns": warns}}, upsert=True
+    )
+
+
+async def remove_warns(chat_id: int, name: str) -> bool:
+    warnsd = await get_warns(chat_id)
+    name = name.lower().strip()
+    if name in warnsd:
+        del warnsd[name]
+        await warnsdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"warns": warnsd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+
 # ban
 @app.on_message(
     filters.command(["ban"]) & ~filters.private & ~BANNED_USERS
