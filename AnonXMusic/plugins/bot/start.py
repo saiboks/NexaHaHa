a8f6781ab0c4b25fa1566221b4a922411383dc75
saiboks <botssai69@gmail.com>
2024-10-17 22:06:40 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -147,7 +147,6 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
                     caption=_["start_4"].format(
                         message.from_user.first_name,
                         app.mention,
