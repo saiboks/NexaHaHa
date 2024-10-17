6af546f065c8d4aac8feeac2927a0f5061be460d
saiboks <botssai69@gmail.com>
2024-10-18 02:37:54 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -59,7 +59,7 @@ async def start_pm(client, message: Message, _):
                 channel = result["channel"]["name"]
                 link = result["link"]
                 published = result["publishedTime"]
-            searched_text = _["start_7"].format(
+            searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
             key = InlineKeyboardMarkup(
@@ -87,18 +87,8 @@ async def start_pm(client, message: Message, _):
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
-
-
-        )  
-
-             # Ab start_3 caption ko reply_markup ke saath bhejo
-        await message.reply_text(
-            text=_["start_3"].format(app.mention),  # Dusra caption (start_3)
-            reply_markup=InlineKeyboardMarkup(out),  # reply_markup ke saath
-
-
-        )  
-
+            reply_markup=InlineKeyboardMarkup(out),
+        )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
@@ -132,11 +122,11 @@ async def welcome(client, message: Message):
                     pass
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
-                    await message.reply_text(_["start_5"])
+                    await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
-                        _["start_6"].format(
+                        _["start_5"].format(
                             app.mention,
                             f"https://t.me/{app.username}?start=sudolist",
                             config.SUPPORT_CHAT,
@@ -148,7 +138,7 @@ async def welcome(client, message: Message):
                 out = start_panel(_)
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
-                    caption=_["start_4"].format(
+                    caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
                         message.chat.title,
